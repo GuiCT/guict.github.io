caa7190bb41d85bee707b220b2cf7bfa7f2f02c2
GuiCT49 <gtomiasi@gmail.com>
2021-08-19 16:52:00 -0300

    adding name and two first projects

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+function aboutMe() {
+    console.log("Show information about me.");
+}
+
+function ecommerce() {
+    location.href = "https://guict49.github.io/portfolio-ecommerce-1/";
+}
+
+function solitaire() {
+    location.href = "https://guict49.github.io/SolitaireGame";
+}
